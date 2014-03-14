5d4d0dea41777f9f66c4263127438fa378b4d6a8
antorof <antorof.dev@gmail.com>
2014-03-14 13:17:35 +0100

    Corrección de errores
    Errores raros inexplicables por mi parte

--- Assets/Scripts/Cohete.js
+++ Assets/Scripts/Cohete.js
@@ -8,9 +8,9 @@ var smooth: float = 0; // Suavidad de cambio
 var GetAxisH: float = 0;
 var GetAxisV: float = 0;
 
-/**
- *	Comprueba si el cohete esta fuera de la zona jugable 
- */
+var pinchado:boolean = false;
+var dir:int;
+
 function estaFuera (){
 	if (transform.position.x < 0 || transform.position.x > 100 ||
 		transform.position.y < 0 || transform.position.y > 60 ) {
@@ -18,10 +18,6 @@ function estaFuera (){
 	}
 	else return false;
 }
-
-/**
- *	Calibra los ejes de inclinacion
- */
 function ResetAxes () {
 	prevAcel = Input.acceleration;
 	acel = Vector3.zero;
@@ -32,21 +28,17 @@ function Start () {
 }
 
 function OnCollisionEnter(collision : Collision) {
-		Debug.Log("Cohete-collision");
-		if(collision.gameObject.tag=="Meta"){
-			print("WoW");
-		}
-		else
 		Application.LoadLevel("mainmenu");
+		print(collision.gameObject.tag);
 }
     
 function Update () { 
 	acel = Vector3.Lerp(acel, Input.acceleration-prevAcel, Time.deltaTime/smooth);
-	GetAxisV = Mathf.Clamp(acel.y * sensV, -1, 1); // Inclinacion Vertical
-	GetAxisH = Mathf.Clamp(acel.x * sensH, -1, 1); // Inclinacion Horizontal
+	GetAxisV = Mathf.Clamp(acel.y * sensV, -1, 1);
+	GetAxisH = Mathf.Clamp(acel.x * sensH, -1, 1);
 
-	transform.Translate(GetAxisH,0,0); // horizontal translation
-	transform.Translate(0,-0.05,0);    // simple gravity simulation 
+	transform.Translate(GetAxisH,0,0);
+	transform.Translate(0,-0.05,0);
 	
 	if(estaFuera()){
 		Application.LoadLevel("mainmenu");
--- Assets/Scripts/toucher.js
+++ Assets/Scripts/toucher.js
@@ -4,16 +4,17 @@ var sistemaParticulas : ParticleSystem;
 var cohete : GameObject;
 
 function Start () {
-
+	
 }
 
 function Update () {
     var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
     var hit : RaycastHit;
 	var ok = collider.Raycast (ray, hit, 100.0F); // el 3er param es la profundidad
-
+	
     if (ok) {
     	if (Input.GetMouseButton(0)) {
+	    	Debug.Log("Me has dado!");
 			cohete.transform.Translate(0,0.2,0);
 	    	sistemaParticulas.emissionRate = 20;
     	}
