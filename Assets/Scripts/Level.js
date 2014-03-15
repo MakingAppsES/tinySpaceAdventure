13d850a3b579a9d6a2f64cd5b6f6a4327940b6af
antorof <antorof.dev@gmail.com>
2014-03-15 20:17:07 +0100

    Limpieza
    Cambiado script de level1 a level y cambio en el lenguaje de
    programación.
    Eliminados materiales innecesarios.

---
+++ Assets/Scripts/Level.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var s_w : int ;
+var s_h : int ;
+var finished : boolean;
+
+function Start () {
+	if (Input.GetKeyDown(KeyCode.Escape))
+		Application.LoadLevel("mainmenu");
+	finished = false;
+	s_w = Screen.width;
+	s_h = Screen.height;
+}
+
+function Update () {
+	
+}
+
+
+function OnGUI () {
+	if (finished) {
+		GUI.Box (Rect (s_w/4,s_h/4,s_w/2,s_h/2), "Nivel terminado");
+	}
+}
