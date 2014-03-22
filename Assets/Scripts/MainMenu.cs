3ee8199a9e5860ec2b80634b8e255a736f76e265
antorof <antorof.dev@gmail.com>
2014-03-22 19:31:16 +0100

    New level0, main menu improvements
    Added: level 0, with simple in-game instructions
    Improved: main menu with locked levels and reset button to reset
    progress.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,12 +5,16 @@ public class MainMenu : MonoBehaviour {
 	
 	public static int s_width;
 	public static int s_height;
-	public Texture2D tittleImg;
+	public Texture2D tittleImg; // Titulo-logo del juego
+	public Texture2D candado; // Imagen de un candado
+	private int nivelAlcanzado;
+	private static int numNiveles = 2; // Numero total de niveles
 
 	// Use this for initialization
 	void Start () {
 		s_width = Screen.width;
 		s_height = Screen.height;
+		nivelAlcanzado = PlayerPrefs.GetInt("nivelAlcanzado", -1);
 		print ("Lvl: "+PlayerPrefs.GetInt("nivelAlcanzado", -1));
 	}
 	
@@ -22,16 +26,32 @@ public class MainMenu : MonoBehaviour {
 	
 	void OnGUI () {
 		int b_width = 200; // Button width
-		int b_height = 50; // Button height
-		
+		int b_height = s_height/10; // Button height
+		int y_base_button = s_height / 5 + tittleImg.height + 10;
+
 		GUI.DrawTexture (new Rect ((s_width-tittleImg.width)/2,(s_height)/5,tittleImg.width,tittleImg.height), 
 		           tittleImg);
 
-		if(GUI.Button(new Rect ((s_width-b_width)/2,(s_height)/5+tittleImg.height+10,b_width,b_height), "Level 1")){
-			Application.LoadLevel("level1");
+		// Level 0 Button
+		if(GUI.Button(new Rect ((s_width-b_width)/2,y_base_button,b_width,b_height), "Level 0")){
+			Application.LoadLevel("level0");
 		}
-		/*if(GUI.Button(new Rect ((s_width-b_width)/2,(s_height)/2+b_height+10,b_width,b_height), "Scene 2")){
-			Debug.Log("Btn 2");
-		}*/
+		
+		// Level 1 Button
+		if(GUI.Button(new Rect ((s_width-b_width)/2,y_base_button+(b_height+10)*1,b_width,b_height), "Level 1")){
+			if (nivelAlcanzado >= 0)
+				Application.LoadLevel("level1");
+		}
+
+		if(nivelAlcanzado != numNiveles-1)
+			for(int i=nivelAlcanzado; i<numNiveles-2;i++)
+				GUI.DrawTexture (new Rect ((s_width-b_width)/2,y_base_button+(b_height+10)*(i+2),candado.width,candado.height), 
+		                 candado);
+			
+		// Reset nivelAlcanzado Button
+		if(GUI.Button(new Rect ((s_width-70)/2,s_height-30-5,70,30), "Reset")){
+			PlayerPrefs.SetInt("nivelAlcanzado", -1);
+			Application.LoadLevel("mainmenu");
+		}
 	}
 }
---
+++ Assets/Scripts/Tutorial.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+	GUI.Label(Rect(5,5,Screen.width,Screen.height),
+	          "> Incline the phone to move the spaceship to the right and to the left");
+	GUI.Label(Rect(5,20,Screen.width,Screen.height),
+	          "> Touch the screen in order to propel the spaceship");
+	GUI.Label(Rect(5,35,Screen.width,Screen.height),
+	          "> Find the goal and land the spaceship there");
+}
